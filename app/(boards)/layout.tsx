741a498ea903ce4b4aabfdf11ad263a5c7c786f6
Kelvin Ochubili <kelvinochubili@gmail.com>
2026-02-04 09:51:50 +0100

    feat: add sidebar provider and navbar component  to main layout

--- app/(boards)/layout.tsx
+++ app/(boards)/layout.tsx
@@ -1,3 +1,16 @@
+import { SidebarProvider } from '@/components/ui/sidebar';
+import { AppSidebar } from '@/components/app-sidebar';
+
+import Navbar from '@/components/navbar';
+
 export default function BoardsLayout({ children }: { children: React.ReactNode }) {
-	return <main>{children}</main>;
+	return (
+		<SidebarProvider>
+			<AppSidebar />
+			<div className='w-full flex flex-col'>
+				<Navbar />
+				<main>{children}</main>
+			</div>
+		</SidebarProvider>
+	);
 }
